9e0254c1204fcefc1ef31a59512d7026ce4d6e54
William <william11616@hotmail.com>
2022-04-28 16:58:43 +0800

    enable logger

--- routes/sns.js
+++ routes/sns.js
@@ -3,22 +3,19 @@ const router = express.Router();
 
 function logger(req, res, next) {
     console.log("logger start")
-    // console.log(req.body);
-    // console.log("SubscribeURL: \n", req.body.SubscribeURL);
+    console.log(req.body);
     console.log("baseUrl = ", req.baseUrl);
     console.log("path = ", req.path);
     console.log("ip = ", req.ip);
     next();
 }
 
-// router.use(logger);
+router.use(logger);
 
 router.get("/", (req, res) => {
     res.status(200).send("SNS")
 });
 router.post("/", (req, res) => {
-    console.log("SNS publish message to /sns path");
-    console.log(req);
     res.status(200).send("SNS Post");
 });
 
